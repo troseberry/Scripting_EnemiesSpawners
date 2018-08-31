b9ded9d1c65d08bca7348c3d44202cc4c33c1523
Troseberry <tayroseberry@gmail.com>
2018-08-30 22:15:58 -0500

    Implemented regions for spawners

--- EnemiesAndSpawners/Assets/Scripts/Components/Spawner.cs
+++ EnemiesAndSpawners/Assets/Scripts/Components/Spawner.cs
@@ -4,40 +4,45 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour 
 {
-   // Controls what the spawner WILL spawn
-   public List<GameObject> prefabList = new List<GameObject>(); 
+    // Controls what the spawner WILL spawn
+    public List<GameObject> prefabList = new List<GameObject>(); 
 
-   // Will control how many and how often; 
-   public uint maxAliveAtOnce = 1; 
-   public float initialDelay = 0.0f; 
-   public float delayBetweenSpawns = 0.0f; 
+    // Will control how many and how often; 
+    public uint maxAliveAtOnce = 1; 
+    public float initialDelay = 0.0f; 
+    public float delayBetweenSpawns = 0.0f; 
 
-   // Controls when the object will spawn; 
-   public bool spawnOnlyWhenVisible = true; 
-   public float spawnerSize = 1.0f; 
+    // Controls when the object will spawn; 
+    public bool spawnOnlyWhenVisible = true; 
+    public float spawnerSize = 1.0f; 
 
-   // internal controls for the system
-   private uint aliveCount = 0; 
-   private Timer spawnTimer = new Timer(); 
+    // internal controls for the system
+    private uint aliveCount = 0; 
+    private Timer spawnTimer = new Timer();
+    private Vector3 spawnLocation;
 
-   //------------------------------------------------------------------------
-   void Start() 
-   {
-      // let people know when they forgot to set this up; 
-      if (prefabList.Count == 0) {
-         Debug.LogWarning( "No prefabs set for spawner: " + gameObject.name ); 
-      }
+    // controls where the spawner will instantiate objs
+    public List<Bounds> spawnRegions;
 
-      spawnTimer.Start(initialDelay); 
-   }
+    //------------------------------------------------------------------------
+    void Start() 
+    {
+        // let people know when they forgot to set this up; 
+        if (prefabList.Count == 0) {
+            Debug.LogWarning( "No prefabs set for spawner: " + gameObject.name ); 
+        }
 
-   //------------------------------------------------------------------------
-   bool ShouldSpawn()
-   {
-      return spawnTimer.HasElapsed()
-         && (aliveCount < maxAliveAtOnce)
-         && IsVisible(); 
-   }
+        spawnTimer.Start(initialDelay);
+        spawnLocation = transform.position;
+    }
+
+    //------------------------------------------------------------------------
+    bool ShouldSpawn()
+    {
+        return spawnTimer.HasElapsed()
+            && (aliveCount < maxAliveAtOnce)
+            && IsVisible(); 
+    }
 
    //------------------------------------------------------------------------
    bool IsVisible()
@@ -60,53 +65,76 @@ public class Spawner : MonoBehaviour
       }
    }
 
-   //------------------------------------------------------------------------
-   void Spawn()
-   {
-      if (prefabList.Count == 0) {
-         return; 
-      }
+    //------------------------------------------------------------------------
+    void Spawn()
+    {
+        if (prefabList.Count == 0) {
+            return; 
+        }
+
+        if (spawnRegions.Count > 0)
+        {
+            spawnLocation = spawnRegions[Random.Range(0, spawnRegions.Count)].center;
+        }
       
-      ++aliveCount; 
+        ++aliveCount;
 
-      // TODO: Give options for HOW it spawns; 
-      int idx = Random.Range( 0, prefabList.Count ); 
-      GameObject prefab = prefabList[idx]; 
-      GameObject go = GameObject.Instantiate( prefab, transform.position, Quaternion.identity ); 
+        // TODO: Give options for HOW it spawns; 
+        int idx = Random.Range( 0, prefabList.Count ); 
+        GameObject prefab = prefabList[idx]; 
+        //GameObject go = GameObject.Instantiate( prefab, transform.position, Quaternion.identity );
+        GameObject go = GameObject.Instantiate(prefab, spawnLocation, Quaternion.identity);
 
-      SpawnerChild child = go.AddComponent<SpawnerChild>(); 
-      child.spawner = this; 
+        SpawnerChild child = go.AddComponent<SpawnerChild>(); 
+        child.spawner = this; 
 
-      spawnTimer.Start( delayBetweenSpawns ); 
-   }
-
-   //------------------------------------------------------------------------
-   public void ObjectDestroyed()
-   {
-      if (aliveCount > 0) {
-         aliveCount--; 
-      }
-   }
+        spawnTimer.Start( delayBetweenSpawns ); 
+    }
 
    //------------------------------------------------------------------------
-   // Displays information to the Scene view - will show
-   // if it is currently visible, as well as an Icon to visualize this
-   // "hidden" object; 
-   public void OnDrawGizmos()
-   {
-      if (spawnOnlyWhenVisible) {
-         Camera c = Camera.main; 
-         Vector3 size = new Vector3(spawnerSize, spawnerSize, spawnerSize);
-         Bounds b = new Bounds( transform.position, size ); 
-         if (c.IsVisible(b)) {
-            Gizmos.color = Color.green; 
-         } else {
-            Gizmos.color = Color.red; 
-         } 
+    public void ObjectDestroyed()
+    {
+        if (aliveCount > 0) {
+            aliveCount--; 
+        }
+    }
 
-         Gizmos.DrawCube( transform.position, size ); 
-      }
+    //------------------------------------------------------------------------
+    // Displays information to the Scene view - will show
+    // if it is currently visible, as well as an Icon to visualize this
+    // "hidden" object; 
+    public void OnDrawGizmos()
+    {
+
+        if (spawnOnlyWhenVisible)
+        {
+            Camera c = Camera.main; 
+            Vector3 size = new Vector3(spawnerSize, spawnerSize, spawnerSize);
+            Bounds b = new Bounds( transform.position, size ); 
+            if (c.IsVisible(b)) {
+                Gizmos.color = Color.green; 
+            } else {
+                Gizmos.color = Color.red; 
+            }
+
+
+            Gizmos.DrawCube( transform.position, size ); 
+        }
 
       Gizmos.DrawIcon( transform.position, "neko.png" ); 
    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Vector3 iconSize = new Vector3(spawnerSize, spawnerSize, spawnerSize);
+
+        for (int i = 0; i < spawnRegions.Count; i++)
+        {
+            Vector3 regionCenter = spawnRegions[i].center;
+            Vector3 regionSize = spawnRegions[i].extents;
+
+            Gizmos.DrawWireCube(regionCenter, regionSize);
+        }
+    }
 }
